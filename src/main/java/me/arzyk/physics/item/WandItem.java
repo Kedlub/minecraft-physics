ef83cda66b8624dcec820d9b820aa83c3ab02840
Kedlub <jakubkyzr@gmail.com>
2022-10-24 16:14:14 +0200

    Enhanced Physics Wand
    Now it "awakens" all connected blocks of the same type (but max of 50)

--- src/main/java/me/arzyk/physics/item/WandItem.java
+++ src/main/java/me/arzyk/physics/item/WandItem.java
@@ -2,20 +2,19 @@ package me.arzyk.physics.item;
 
 import me.arzyk.physics.entity.PhysicsBlockEntity;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WandItem extends Item {
+
+    static final int MAX_BLOCK_COUNT = 50;
 
     public WandItem(Settings settings) {
         super(settings);
@@ -27,9 +26,30 @@ public class WandItem extends Item {
 
         if(!world.isClient()) {
             BlockState block = world.getBlockState(blockPos);
-            PhysicsBlockEntity.spawnFromBlock(world, blockPos, block);
+            awakenBlocks(world, blockPos, block);
         }
 
+        blockPosList.clear();
+        blockCount = 0;
+
         return ActionResult.success(world.isClient);
+    }
+
+    List<BlockPos> blockPosList = new ArrayList<>();
+    int blockCount = 0;
+
+    public void awakenBlocks(World world, BlockPos blockPos, BlockState block) {
+        blockCount++;
+        if(blockCount > MAX_BLOCK_COUNT)
+            return;
+
+        blockPosList.add(blockPos);
+        for (Direction direction : Direction.values()) {
+            BlockPos offsetPos = blockPos.offset(direction);
+            if(world.getBlockState(offsetPos).getBlock().equals(block.getBlock()) && !blockPosList.contains(offsetPos)) {
+                awakenBlocks(world, offsetPos, block);
+            }
+        }
+        PhysicsBlockEntity.spawnFromBlock(world, blockPos, block);
     }
 }
